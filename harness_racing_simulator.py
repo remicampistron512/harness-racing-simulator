5753d62fb9241ee2fd23d1d6fcbe5a21a2c133cf
Rémi Campistron <remi.campistron@fms-ea.com>
2025-10-15 15:40:12 +0200

    Affiche le déroulé de la course de façon graphique + docstrings

--- harness_racing_simulator.py
+++ harness_racing_simulator.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 """
-Simulateur de trot attelé, il s'agit d'une course de 12 à 20 chevaux, faisant l'objet d'un tierce,quarte,quinte, elle
-se deroule sur un hippodrome rectiligne de 2400m.
+Simulateur de trot attelé, il s'agit d'une course de 12 à 20 chevaux, faisant l'objet d'un tiercé, quarté, quinte, elle
+se déroule sur un hippodrome rectiligne de 2400m.
 """
 
 import random
-
+from math import floor
+# Le dictionnaire contenant les evolutions de la vitesse d'un cheval
 SPEED_CHART : dict = {
     0: ( 0,  1,  1,  1,  2,  2),
     1: ( 0,  0,  1,  1,  1,  2),
@@ -16,30 +17,142 @@ SPEED_CHART : dict = {
     4: (-1, -1,  0,  0,  0,  1),
     5: (-2, -1,  0,  0,  0,  1),
     6: (-2, -1,  0,  0,  0, None),
-                    }
+                   }
+# Le tableau des distances parcourues selon le jet de dé
 DISTANCE_CHART = [0,23,46,69,92,115,138]
 
 
-def init_game(num_horses):
+def cross_multiplication(a,b,c):
+    """
+    Produit en croix arrondi
+    :param a:
+    :param b:
+    :param c:
+    :return:
+    """
+    return floor((b * c) / a)
+
+
+def start_game_graphical(horses,race_type):
+    """
+    La fonction principale d'affichage du jeu en version graphique
+    :param horses:
+    :param race_type:
+    :return:
+    """
+    horse_finished_count = 0
+    finishing_line = []
+    number_of_disqualifications = 0
+    turns_count = 0
+    race_result = []
+    while True:
+        if turns_count != 0:
+            print("la course a commencé il y a " + str(turns_count * 10) + " sec")
+            for horse in horses:
+                distance_remaining = 2400 - horse["distance"]
+                distance_remaining_gui = cross_multiplication(2400,120,distance_remaining)
+                distance_gui = cross_multiplication(2400,120,horse["distance"])
+                if horse["distance"]  > 0:
+                    print(" |" + " " * distance_gui + "♘" + " " * distance_remaining_gui + "|")
+                else:
+                    print("♘ |" + " " * 120 + "|")
+                if not horse["disqualified"] and not horse["finished"]:
+                    print(
+                        f"Le cheval {horse["id"]} a une vitesse de {horse["speed"]} et a parcouru une distance de {horse["distance"]}m ")
+                elif not horse["disqualified"] and horse["finished"]:
+                    print(f"Le cheval {horse["id"]} a fini la course")
+                elif horse["disqualified"] and not horse["finished"]:
+                    print(f"Le cheval {horse["id"]} a été disqualifié !")
+        else:
+            print("Les chevaux sont au départ")
+            for _ in horses:
+                print("♘ |" + " " * 120  +"|")
+
+        for horse in horses:
+            if horse["finished"] is False:
+                speed_change = change_speed(horse["speed"], roll_die(6))
+                if speed_change is not None:
+                    horse["speed"] += speed_change
+                else:
+                    horse["disqualified"] = True
+                    number_of_disqualifications = + 1
+                distance = DISTANCE_CHART[horse["speed"]]
+                horse["distance"] += distance
+                if horse["distance"] > 2400 and not horse["disqualified"]:
+                    horse["finished"] = True
+                    horse_finished_count += 1
+                    finishing_line.append(horse["id"])
+
+        if horse_finished_count >= len(horses) - 1 - number_of_disqualifications:
+            break
+        # selon le type de course on sélectionne 3,4,5 chevaux
+        match race_type:
+            case "quinte":
+                race_result = finishing_line[:5]
+            case "quarte":
+                race_result = finishing_line[:4]
+            case "tierce":
+                race_result = finishing_line[:3]
+
+        turns_count += 1
+        next_turn()
+
+    print(f"Tous les chevaux sont arrivés, la course est terminée.")
+    print(f"Le {race_type} gagnant est le suivant ")
+    print(", ".join(map(str, race_result)))
+
+
+def init_game(num_horses,gui=False):
+    """
+    initialise la course en passant le nombre de chevaux et le type de la course
+    :param num_horses:
+    :param gui:
+    :return:
+    """
     horses = init_horses(num_horses)
     race_type = ask_horse_race_type()
-    start_game(horses,race_type)
+    if gui:
+        start_game_graphical(horses,race_type)
+    else:
+        start_game(horses,race_type)
 
 
 def change_speed(speed,die_roll):
+    """
+    Changement de la vitesse du cheval en se basant sur le tableau des vitesses
+    :param speed:
+    :param die_roll:
+    :return:
+    """
     return SPEED_CHART[speed][die_roll - 1]
 
 
 
 def roll_die(die_size):
+    """
+    Fonction qui permet de lancer un dé à n faces
+    :param die_size: nombre de faces
+    :return:
+    """
     return random.randrange(1, die_size+1)
 
 
 def next_turn():
+    """
+    Avance la course
+    :return:
+    """
     input("Appuyez sur entrée pour avancer la course")
 
 
 def start_game(horses,race_type):
+    """
+        La fonction principale d'affichage du jeu en version texte
+        :param horses: la liste des chevaux
+        :param race_type: le type de course
+        :return:
+        """
+
     horse_finished_count = 0
     finishing_line = []
     number_of_disqualifications = 0
@@ -50,7 +163,7 @@ def start_game(horses,race_type):
             print("la course a commencé il y a " + str(turns_count * 10) + " sec")
             for horse in horses:
                 if not horse["disqualified"] and not horse["finished"]:
-                    print(f"Le cheval {horse["id"]} a une vitesse de {horse["speed"]} et a parcouru une distance de {horse["distance"]}m ")
+                    print(f"Le cheval ♘ {horse["id"]} a une vitesse de {horse["speed"]} et a parcouru une distance de {horse["distance"]}m ")
                 elif not horse["disqualified"] and  horse["finished"] :
                     print(f"Le cheval {horse["id"]} a fini la course")
                 elif horse["disqualified"] and not horse["finished"]:
@@ -95,6 +208,11 @@ def start_game(horses,race_type):
 
 
 def init_horses(num_horses) :
+    """
+    Créé un dictionnaire contenant les différents paramètres que l'on désire suivre
+    :param num_horses:
+    :return:
+    """
     horses = []
     for horse in range(1, num_horses + 1):
         horse_dict = {
@@ -109,6 +227,10 @@ def init_horses(num_horses) :
     return horses
 
 def ask_horse_race_type():
+    """
+    Demande le type de course
+    :return:
+    """
     race_type = input("Quel type de course ? (tierce,quarte,quinte) : ")
     if race_type == "quinte" or race_type == "quarte" or race_type == "tierce":
         return race_type
@@ -117,4 +239,4 @@ def ask_horse_race_type():
         return ask_horse_race_type()
 
 if __name__ == '__main__':
-    init_game(12)
+    init_game(12,True)
